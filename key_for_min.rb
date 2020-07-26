fe858119c7bc017e097de44f953f60a16793c460
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-26 04:23:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,4 +5,5 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
  hash.each do |k, v| 
-   if lowest_value == nil || v <
+   if lowest_value == nil || v < lowest_value
+     
